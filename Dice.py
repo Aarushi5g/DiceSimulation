f110ece1a2317f273c89e7a27927f21856630d04
Aarushi5g <aarushigupta55555@gmail.com>
2020-05-15 01:27:08 +0530

    Add simulated figures of dice number rolled

--- Dice.py
+++ Dice.py
@@ -1,9 +1,48 @@
 import random
+dice_dictionary = {
+    1: '''
+          _   _   _
+        |           |
+        |    O    |
+        |_   _  _|
+         ''',
+    2: '''
+          _   _   _
+        |  O      |
+        |      O  |
+        |_   _  _|
+         ''',
+    3: '''
+          _   _   _
+        |O        |
+        |   O     |
+        |_   _O_|
+         ''',
+    4: '''
+          _   _   _
+        |O      O|
+        |            |
+        |O_  _O|
+         ''',
+    5: '''
+           _   _   _
+        |O      O|
+        |    O     |
+        |O_  _O|
+         ''',
+    6: '''
+          _    _    _
+        |O O  O |
+        |O O  O |
+        |O_O_O|
+         '''
+}
 
 
 def rolling():
     dice_roll = random.randint(1, 6)
     print(dice_roll)
+    print(dice_dictionary[dice_roll])
 
 
 rolling()
